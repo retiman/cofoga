36e164a68660aa586533cc68da4666bf9e5c1c53
Min Huang <min.huang@alumni.usc.edu>
2009-07-21 06:42:40 -0700

    Expose more meaningful name to clients

--- src/main/scala/scaga/GameBoard.scala
+++ src/main/scala/scaga/GameBoard.scala
@@ -5,12 +5,12 @@ import scaga.Player._
 
 class GameBoard(val rows: Int, val cols: Int, val connections: Int) {
   require(rows > 0 && cols > 0 && connections > 0)
-  protected var turn    = White
+  protected var player  = White
   protected var board   = new Array[Array[Player]](rows, cols)
   protected var columns = new Array[Int](cols)
   protected var history = new Stack[Int]()
 
-  def player = turn
+  def turn = player
   def apply(row: Int, col: Int) = board(row)(col)
   def moves = columns.filter(_ < rows)
 }
