162c21394d49fb1aced8d8e54a45fd627bebce12
Min Huang <min.huang@alumni.usc.edu>
2009-07-21 08:15:48 -0700

    Winner check

--- src/main/scala/scaga/Winnable.scala
+++ src/main/scala/scaga/Winnable.scala
@@ -8,14 +8,36 @@ trait Winnable {
   val connections: Int
   protected val board: Array[Array[Player]]
 
-  def horizontal(row: Int, col: Int) = {
-    var count  = 0
-    val player = board(row)(col)
-    val start  = 0    max (col - connections + 1)
-    val end    = cols min (col + connections)
-    for (j <- start until end
-         if board(row)(j) == player && count < connections)
+  def check(player: Player, is: Iterable[Int], js: Iterable[Int]) = {
+    var count = 0
+    for (i <- is;
+         j <- js
+         if (board(i)(j)) == player && count < connections)
       count += 1
     if (count == connections) Some(player) else None
   }
+
+  def horizontal(row: Int, col: Int) = check(
+    board(row)(col),
+    row to row,
+    (0 max col - connections + 1) until (cols min col + connections)
+  )
+
+  def vertical(row: Int, col: Int) = check(
+    board(row)(col),
+    (0 max row - connections + 1) until (rows min row + connections),
+    col to col
+  )
+
+  def upforward(row: Int, col: Int) = check(
+    board(row)(col),
+    (0 max row - connections + 1) until (rows min row + connections - 1),
+    (0 max col - connections + 1) until (cols min col + connections)
+  )
+
+  def downforward(row: Int, col: Int) = check(
+    board(row)(col),
+    (rows min row + connections - 1) until (0 max row - connections + 1) by -1,
+    (0 max col - connections + 1) until (cols min col + connections)
+  )
 }
