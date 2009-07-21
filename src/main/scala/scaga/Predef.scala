b19a4c13157a0d1ac3e3e97c642a7e1961129e5f
Min Huang <min.huang@alumni.usc.edu>
2009-07-21 08:26:27 -0700

    Convenience functions for pairs

--- src/main/scala/scaga/Predef.scala
+++ src/main/scala/scaga/Predef.scala
@@ -1,6 +1,11 @@
 package scaga
 
 object Predef {
+  implicit def pairWrapper[A, B](a: Pair[A, B]) = new {
+    def fst = a._1
+    def snd = a._2
+  }
+
   def rescue[E](expr: => E) = new {
     def using[A >: E](alt: A) = try { if (expr == null) alt else expr } catch {
       case e: Exception => alt
