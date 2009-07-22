4a1db3fafa6ff35e5333c75294f4a883728dbbda
Min Huang <min.huang@alumni.usc.edu>
2009-07-22 02:19:18 -0700

    Cleaner version of NaiveEvaluation

--- src/main/scala/scaga/eval/naive/NaiveEvaluation.scala
+++ src/main/scala/scaga/eval/naive/NaiveEvaluation.scala
@@ -7,12 +7,18 @@ import scaga.Predef._
 trait NaiveEvaluation extends EvaluationStrategy {
   type Threat = List[Pair[Int, Int]]
   val pattern = """X+|O+""".r
+  val limit   = board.rows max board.cols
+
+  def horizontalThreats(i: Int)      = for (j <- 0 until board.cols) yield board(i)(j)
+  def verticalThreats(j: Int)        = for (i <- 0 until board.rows) yield board(i)(j)
+  def diagonallyUpThreats(i: Int)    = for (k <- 0 until limit if board.contains(i + k)(k)) yield board(i + k)(k)
+  def diagonallyDownThreats(j: Int)  = for (k <- 0 until limit if board.contains(k)(j - k)) yield board(k)(j - k)
 
   def score = {
     val whites = new Array[Int](board.connections - 1)
     val blacks = new Array[Int](board.connections - 1)
 
-    def compute(s: String) = pattern.findAllIn(s).foreach { m =>
+    def weigh(s: String) = pattern.findAllIn(s).foreach { m =>
       valueOf(m.toString.charAt(0).toString) match {
         case Some(White) => whites(m.length) += 1
         case Some(Black) => blacks(m.length) += 1
@@ -20,36 +26,10 @@ trait NaiveEvaluation extends EvaluationStrategy {
       }
     }
 
-    // Compute horizontal threats
-    for (i <- 0 until board.rows) {
-      val r = for (j <- 0 until board.cols) yield board(i)(j)
-      compute(r.mkString)
-    }
-
-    // Compute vertical threats
-    for (j <- 0 until board.cols) {
-      val r = for (i <- 0 until board.rows) yield board(i)(j)
-      compute(r.mkString)
-    }
-
-    // Compute up diagonal threats
-    val limit = board.rows max board.cols
-    for (i <- (board.cols - 1) to 0 by -1) {
-      val r = for (k <- 0 until limit if
-                   (0 until board.rows contains i + k) &&
-                   (0 until board.cols contains k))
-                yield board(i + k)(k)
-      compute(r.mkString)
-    }
-
-    // Compute down diagonal threats
-    for (j <- 0 until board.cols) {
-      val r = for (k <- 0 until limit if
-                   (0 until board.rows contains k) &&
-                   (0 until board.cols contains j - k))
-                yield board(k)(j - k)
-      compute(r.mkString)
-    }
+    for (i <- 0 until board.rows)          weigh(horizontalThreats(i).mkString)
+    for (j <- 0 until board.cols)          weigh(verticalThreats(j).mkString)
+    for (i <- (board.cols - 1) to 0 by -1) weigh(diagonallyUpThreats(i).mkString)
+    for (j <- 0 until board.cols)          weigh(diagonallyDownThreats(j).mkString)
 
     whites.indices.map(i => Math.pow(3, i) * whites(i)).reduceLeft(_+_) -
     blacks.indices.map(i => Math.pow(3, i) * blacks(i)).reduceLeft(_+_)
