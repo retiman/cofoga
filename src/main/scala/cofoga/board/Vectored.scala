7ccf4edd5f72ef590c107cf5ebbb521be742bb3e
Min Huang <min.huang@alumni.usc.edu>
2009-07-23 14:42:29 -0700

    Rem generalization; for test, just fill tested area with Player, and Neither for rest

--- src/main/scala/cofoga/board/Vectored.scala
+++ src/main/scala/cofoga/board/Vectored.scala
@@ -1,35 +1,35 @@
 package cofoga.board
 
-trait Vectored[A] {
-  def matrix: Array[Array[A]]
+trait Vectored {
+  def matrix: Array[Array[Player]]
   def rows: Int
   def cols: Int
   def containsRow(row: Int) = 0 until rows contains row
   def containsCol(col: Int) = 0 until cols contains col
   def contains(row: Int)(col: Int) = containsRow(row) && containsCol(col)
 
-  def horizontal(row: Int, col: Int)(end: Int): Iterable[Pair[Int, Int]] = end match {
+  def horizontal(row: Int, col: Int)(end: Int): Iterable[Player] = end match {
     case end if end < 0 => val k = -end
                            horizontal(row, col - k + 1)(-end)
     case _              => for (j <- col until col + end if containsCol(j))
                              yield matrix(row)(j)
   }
 
-  def vertical(row: Int, col: Int)(end: Int): Iterable[Pair[Int, Int]] = end match {
+  def vertical(row: Int, col: Int)(end: Int): Iterable[Player] = end match {
     case end if end < 0 => val k = -end
                            vertical(row - k + 1, col)(-end)
     case _              => for (i <- row until end if containsRow(i))
                              yield matrix(i)(col)
   }
 
-  def diagonalup(row: Int, col: Int)(end: Int): Iterable[Pair[Int, Int]] = end match {
+  def diagonalup(row: Int, col: Int)(end: Int): Iterable[Player] = end match {
     case end if end < 0 => val k = -end
                            diagonalup(row - k + 1, col - k + 1)(-end)
     case _              => for (k <- 0 until end if contains(row + k)(col + k))
                              yield matrix(row + k)(col + k)
   }
 
-  def diagonaldown(row: Int, col: Int)(end: Int): Iterable[Pair[Int, Int]] = end match {
+  def diagonaldown(row: Int, col: Int)(end: Int): Iterable[Player] = end match {
     case end if end < 0 => val k = -end
                            diagonaldown(row + k - 1, col - k + 1)(-end)
     case _              => for (k <- 0 until end if contains(row - k)(col + k))
