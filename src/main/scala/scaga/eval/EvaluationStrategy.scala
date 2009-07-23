2eece938b14dbd11fd8168cc1b5955d48e037e37
Min Huang <min.huang@alumni.usc.edu>
2009-07-22 18:40:50 -0700

    Tests for horizontal/vertical threat calculation

--- src/main/scala/scaga/eval/EvaluationStrategy.scala
+++ src/main/scala/scaga/eval/EvaluationStrategy.scala
@@ -1,8 +1,8 @@
 package scaga.eval
 
-import java.lang.Double._
-
 trait EvaluationStrategy {
+  val POSITIVE_INFINITY = java.lang.Double.POSITIVE_INFINITY
+  val NEGATIVE_INFINITY = java.lang.Double.NEGATIVE_INFINITY
   val board: GameBoard
   def score: Double
 }
--- src/test/scala/scaga/eval/naive/NaiveEvaluationSpec.scala
+++ src/test/scala/scaga/eval/naive/NaiveEvaluationSpec.scala
@@ -7,5 +7,28 @@ import scaga.Player._
 class NaiveEvaluationSpecTest extends JUnit4(NaiveEvaluationSpec)
 
 object NaiveEvaluationSpec extends Specification {
-  
+  class TestBoard extends GameBoard {
+    def grid = matrix
+  }
+
+  class TestEvaluation(val board: TestBoard) extends NaiveEvaluation
+
+  "horizontals calculation" should {
+    "compute correct threats" in {
+      val board = new TestBoard()
+      for (j <- 0 until board.cols if j % 2 == 0)
+        board.grid(0)(j) = White
+      val eval = new TestEvaluation(board)
+      "O-O-O-O" mustEqual eval.horizontalThreats(0).map(_.format).mkString
+    }
+  }
+  "verticals calculation" should {
+    "compute correct threats" in {
+      val board = new TestBoard()
+      for (i <- 0 until board.rows if i % 2 == 0)
+        board.grid(i)(0) = White
+      val eval = new TestEvaluation(board)
+      "O-O-O-" mustEqual eval.verticalThreats(0).map(_.format).mkString
+    }
+  }
 }
