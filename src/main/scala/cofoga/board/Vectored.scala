1f97888115d24a5f2959a0beff3fc521fb605ecd
Min Huang <min.huang@alumni.usc.edu>
2009-07-23 14:12:15 -0700

    Made relationship b/t row/col calculation and -end clearer

--- src/main/scala/cofoga/board/Vectored.scala
+++ src/main/scala/cofoga/board/Vectored.scala
@@ -11,25 +11,29 @@ trait Vectored {
   def contains(row: Int)(col: Int) = containsRow(row) && containsCol(col)
 
   def horizontal(row: Int, col: Int)(end: Int): Iterable[Pair[Int, Int]] = end match {
-    case end if end < 0 => horizontal(row, col + end + 1)(-end)
+    case end if end < 0 => val k = -end
+                           horizontal(row, col - k + 1)(-end)
     case _              => for (j <- col until col + end if containsCol(j))
                              yield matrix(row)(j)
   }
 
   def vertical(row: Int, col: Int)(end: Int): Iterable[Pair[Int, Int]] = end match {
-    case end if end < 0 => vertical(row + end + 1, col)(-end)
+    case end if end < 0 => val k = -end
+                           vertical(row - k + 1, col)(-end)
     case _              => for (i <- row until end if containsRow(i))
                              yield matrix(i)(col)
   }
 
   def diagonalup(row: Int, col: Int)(end: Int): Iterable[Pair[Int, Int]] = end match {
-    case end if end < 0 => diagonalup(row + end + 1, col + end + 1)(-end)
+    case end if end < 0 => val k = -end
+                           diagonalup(row - k + 1, col - k + 1)(-end)
     case _              => for (k <- 0 until end if contains(row + k)(col + k))
                              yield matrix(row + k)(col + k)
   }
 
   def diagonaldown(row: Int, col: Int)(end: Int): Iterable[Pair[Int, Int]] = end match {
-    case end if end < 0 => diagonaldown(row - end + 1, col + end + 1)(-end)
+    case end if end < 0 => val k = -end
+                           diagonaldown(row + k - 1, col - k + 1)(-end)
     case _              => for (k <- 0 until end if contains(row - k)(col + k))
                              yield matrix(row - k)(col + k)
   }
