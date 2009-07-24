d75b30699b9ba674833d2598508bc282842c6ec6
Min Huang <min.huang@alumni.usc.edu>
2009-07-23 19:30:21 -0700

    Easier to test

--- src/main/scala/cofoga/eval/NaiveEvaluation.scala
+++ src/main/scala/cofoga/eval/NaiveEvaluation.scala
@@ -16,22 +16,53 @@ trait NaiveEvaluation extends EvaluationStrategy {
   def eval(board: GameBoard) = {
     val whites  = new Array[Int](board.connections - 1)
     val blacks  = new Array[Int](board.connections - 1)
-    for (i <- 0 until board.rows)           weigh(board.horizontal(i, 0)(board.cols), whites, blacks)
-    for (j <- 0 until board.cols)           weigh(board.vertical(0, j)(board.rows),   whites, blacks)
-    for (j <- -board.cols until board.cols) weigh(board.diagup(0, j)(board.rows),     whites, blacks)
-    for (j <- 0 until 2*board.rows)         weigh(board.diagdown(0, j)(board.rows),   whites, blacks)
+    horizontalEvaluation(board, whites, blacks)
+    verticalEvaluation(board, whites, blacks)
+    diagupEvaluation(board, whites, blacks)
+    diagdownEvaluation(board, whites, blacks)
     whites.indices.map(i => Math.pow(3, i) * whites(i)).reduceLeft(_+_) -
     blacks.indices.map(i => Math.pow(3, i) * blacks(i)).reduceLeft(_+_)
   }
 
-  def weigh(players: Seq.Projection[Player], whites: Array[Int], blacks: Array[Int]) = {
-    val s = players.mkString
-    pattern.findAllIn(s).foreach { m =>
+  def weigh(players: String, whites: Array[Int], blacks: Array[Int]) = {
+    pattern.findAllIn(players).foreach { m =>
       valueOf(m(0)) match {
         case Some(White) => whites(m.size) += 1
         case Some(Black) => blacks(m.size) += 1
         case _           => ()
       }
+    }
+  }
+
+  def horizontalEvaluation(board: GameBoard, whites: Array[Int], blacks: Array[Int]) = {
+    for (i <- 0 until board.rows) {
+      val players = board.horizontal(i, 0)(board.cols).mkString
+      log("Evaluating horizontal at row " + i + ": " + players)
+      weigh(players, whites, blacks)
+    }
+  }
+
+  def verticalEvaluation(board: GameBoard, whites: Array[Int], blacks: Array[Int]) = {
+    for (j <- 0 until board.cols) {
+      val players = board.vertical(0, j)(board.rows).mkString
+      log("Evaluating vertical at col " + j + ": " + players)
+      weigh(players, whites, blacks)
+    }
+  }
+
+  def diagupEvaluation(board: GameBoard, whites: Array[Int], blacks: Array[Int]) = {
+    for (j <- -board.cols until board.cols) {
+      val players = board.diagup(0, j)(board.rows).mkString
+      log("Evaluating diagup at col " + j + ": " + players)
+      weigh(players, whites, blacks)
+    }
+  }
+
+  def diagdownEvaluation(board: GameBoard, whites: Array[Int], blacks: Array[Int]) = {
+    for (j <- 0 until 2*board.rows) {
+      val players = board.diagdown(0, j)(board.rows).mkString
+      log("Evaluating diagdown at col " + j + ": " + players)
+      weigh(players, whites, blacks)
     }
   }
 }
