c37958d6126e4846ae8bdd19a05233218f022e4b
Min Huang <min.huang@alumni.usc.edu>
2009-07-23 17:44:42 -0700

    FormattedSpec tests

---
+++ src/test/scala/cofoga/board/FormattedSpec.scala
@@ -0,0 +1,60 @@
+package cofoga.board
+
+import scala.util.logging.ConsoleLogger
+import org.specs.runner.JUnit4
+import org.specs.Specification
+import cofoga.Player._
+import cofoga.board.GameBoard._
+
+class FormattedSpecTest extends JUnit4(FormattedSpec)
+
+object FormattedSpec extends Specification with Formatted
+                                           with ConsoleLogger {
+  val rows = ROWS
+  val cols = COLS
+  val matrix = new Array[Array[Player]](rows, cols)
+  def reset() = for (i <- 0 until rows; j <- 0 until cols) matrix(i)(j) = Neither
+  reset()
+
+  "printing a matrix " should {
+    "output an empty board correctly" in {
+      reset()
+      """5  -  -  -  -  -  -  -
+        |4  -  -  -  -  -  -  -
+        |3  -  -  -  -  -  -  -
+        |2  -  -  -  -  -  -  -
+        |1  -  -  -  -  -  -  -
+        |0  -  -  -  -  -  -  -
+        |   0  1  2  3  4  5  6""".stripMargin mustEqual toString.trim
+    }
+    "output an board with two pieces correctly" in {
+      reset()
+      matrix(0)(3) = White
+      matrix(1)(3) = Black
+      """5  -  -  -  -  -  -  -
+        |4  -  -  -  -  -  -  -
+        |3  -  -  -  -  -  -  -
+        |2  -  -  -  -  -  -  -
+        |1  -  -  -  X  -  -  -
+        |0  -  -  -  O  -  -  -
+        |   0  1  2  3  4  5  6""".stripMargin mustEqual toString.trim
+    }
+    "output a board with winner correctly" in {
+      reset()
+      matrix(0)(0) = White
+      matrix(1)(0) = Black
+      matrix(0)(1) = White
+      matrix(1)(1) = Black
+      matrix(0)(2) = White
+      matrix(1)(2) = Black
+      matrix(0)(3) = White
+      """5  -  -  -  -  -  -  -
+        |4  -  -  -  -  -  -  -
+        |3  -  -  -  -  -  -  -
+        |2  -  -  -  -  -  -  -
+        |1  X  X  X  -  -  -  -
+        |0  O  O  O  O  -  -  -
+        |   0  1  2  3  4  5  6""".stripMargin mustEqual toString.trim
+    }
+  }
+}
