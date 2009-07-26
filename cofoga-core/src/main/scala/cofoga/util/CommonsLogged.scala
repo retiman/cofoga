f55f7abd02e0c31a61afbb9ef65edeff2ce0081a
Min Huang <min.huang@alumni.usc.edu>
2009-07-26 01:50:19 -0700

    Commons logging added

---
+++ cofoga-core/src/main/scala/cofoga/util/CommonsLogged.scala
@@ -0,0 +1,20 @@
+package cofoga.util
+
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
+
+trait CommonsLogged {
+  protected val commonsLog = LogFactory.getLog(this.getClass)
+  protected def log = new {
+    def debug(msg: => String, t: Throwable) = if (commonsLog.isDebugEnabled) commonsLog.debug(msg, t)
+    def debug(msg: => String) = if (commonsLog.isDebugEnabled) commonsLog.debug(msg)
+    def error(msg: => String, t: Throwable) = if (commonsLog.isErrorEnabled) commonsLog.error(msg, t)
+    def error(msg: => String) = if (commonsLog.isErrorEnabled) commonsLog.error(msg)
+    def info(msg: => String, t: Throwable) = if (commonsLog.isInfoEnabled) commonsLog.info(msg, t)
+    def info(msg: => String) = if (commonsLog.isInfoEnabled) commonsLog.info(msg)
+    def trace(msg: => String, t: Throwable) = if (commonsLog.isTraceEnabled) commonsLog.trace(msg, t)
+    def trace(msg: => String) = if (commonsLog.isTraceEnabled) commonsLog.trace(msg)
+    def warn (msg: => String, t: Throwable) = if (commonsLog.isWarnEnabled)  commonsLog.warn(msg, t)
+    def warn (msg: => String) = if (commonsLog.isWarnEnabled)  commonsLog.warn(msg)
+  }
+}
