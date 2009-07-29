587b1411296a344dccf8a1c060e7366f61bf728b
Min Huang <min.huang@alumni.usc.edu>
2009-07-29 02:03:54 -0700

    API change

--- cofoga-core/src/main/scala/cofoga/Vectored.scala
+++ cofoga-core/src/main/scala/cofoga/Vectored.scala
@@ -11,41 +11,33 @@ trait Vectored {
   protected def containsCol(col: Int) = 0 until cols contains col
   protected def contains(row: Int)(col: Int) = containsRow(row) && containsCol(col)
 
-  def hvectors(row: Int, col: Int) = {
+  def horizontal(row: Int)(col: Int) = {
     for (k <- 0 until connections if contains(row)(col + k))
       yield (row, col + k)
   }
 
-  def vvectors(row: Int, col: Int) = {
+  def vertical(row: Int)(col: Int) = {
     for (k <- 0 until connections if contains(row + k)(col))
       yield (row + k, col)
   }
 
-  def ddvectors(row: Int, col: Int) = {
+  def diagup(row: Int)(col: Int) = {
+    for (k <- 0 until connections if contains(row + k)(col + k))
+      yield (row + k, col + k)
+  }
+
+
+  def diagdown(row: Int)(col: Int) = {
     for (k <- 0 until connections if contains(row - k)(col + k))
       yield (row - k, col + k)
   }
 
-  def duvectors(row: Int, col: Int) = {
-    for (k <- 0 until connections if contains(row + k)(col + k))
-      yield (row + k, col + k)
-  }
 
   def _horizontal(row: Int, col: Int)(end: Int): Seq.Projection[Player] = end match {
     case end if end < 0 => val k = -end
                            _horizontal(row, col - k + 1)(-end)
     case _              => for (j <- col until col + end if containsCol(j))
                              yield matrix(row)(j)
-  }
-
-  def computeVectors() = {
-    var vectors = List[Seq.Projection[Pair[Int, Int]]]()
-    for (i <- 0 until rows;
-         j <- 0 until cols) {
-      val threats = Set(hvectors _, vvectors _, ddvectors _, duvectors _).map(f => f(i, j)).filter(_.toList.size == connections)
-      vectors ++= threats
-    }
-    vectors
   }
 
   def _vertical(row: Int, col: Int)(end: Int): Seq.Projection[Player] = end match {
