00076b69ea60590ebec0dd7e317572281ce32de3
Min Huang <min.huang@alumni.usc.edu>
2009-07-29 05:52:34 -0700

    Working but still slow

--- cofoga-core/src/test/scala/cofoga/ContendedSpec.scala
+++ cofoga-core/src/test/scala/cofoga/ContendedSpec.scala
@@ -26,7 +26,6 @@ object ContendedSpec extends Specification with Contended {
       horizontalWinner(0)(3) mustBe true
     }
     "not be detected" in {
-      log.info("h o")
       matrix(0)(0) = White
       matrix(0)(1) = White
       matrix(0)(2) = White
@@ -96,6 +95,27 @@ object ContendedSpec extends Specification with Contended {
       diagdownWinner(3)(3) mustBe false
       diagdownWinner(2)(4) mustBe false
       diagdownWinner(1)(5) mustBe false
+    }
+  }
+
+  "winner computation" should { reset.before
+    "be relatively fast" in {
+      for (i <- 0 until rows; j <- 0 until cols) matrix(i)(j) = White
+      val branchingFactor = 7
+      val states = Math.pow(7, HALF_PLIES)
+      val pruned = states.toInt / 3
+      log.info("Winner computation simulation for " + pruned + " states")
+      val start = System.currentTimeMillis
+      for (i <- 0 until pruned) {
+        horizontalWinner(3)(3)
+        verticalWinner(3)(3)
+        diagupWinner(3)(3)
+        diagdownWinner(3)(3)
+      }
+      val end = System.currentTimeMillis
+      val time = (end - start) / 1000
+      log.info("Winner computation simulation took " + time + " seconds")
+      time <= 5 mustBe true
     }
   }
 }
