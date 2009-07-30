01768c19103e8249c49dc88200bd04b5f0c9489b
Min Huang <min.huang@alumni.usc.edu>
2009-07-30 02:56:15 -0700

    Compiling again

--- cofoga-core/src/main/scala/cofoga/engine/Engine.scala
+++ cofoga-core/src/main/scala/cofoga/engine/Engine.scala
@@ -3,7 +3,7 @@ package cofoga.engine
 import Cofoga._
 import Player._
 
-abstract class Engine(settings: Settings)/* extends CofogaBoard(settings.rows, settings.cols, settings.connections)
+abstract class Engine(settings: Settings) extends GameBoard(settings.rows, settings.cols, settings.connections)
                                           with Search
                                           with Utility
                                           with Logged {
@@ -13,4 +13,4 @@ abstract class Engine(settings: Settings)/* extends CofogaBoard(settings.rows, s
     val m = search(this)
     move(m)
   }
-}*/
+}
