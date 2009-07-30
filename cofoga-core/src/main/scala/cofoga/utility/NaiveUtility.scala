899f579515a852c2a016bc7249173ecbc02882d3
Min Huang <min.huang@alumni.usc.edu>
2009-07-30 01:32:29 -0700

    Tests failing...

--- cofoga-core/src/main/scala/cofoga/utility/NaiveUtility.scala
+++ cofoga-core/src/main/scala/cofoga/utility/NaiveUtility.scala
@@ -3,14 +3,14 @@ package cofoga.utility
 import Cofoga._
 import Player._
 
-trait NaiveUtility extends ThreatGroups with Utility {
+trait NaiveUtility extends Utility {
   def utility = board.winner match {
     case White => POSITIVE_INFINITY
     case Black => NEGATIVE_INFINITY
     case _     => {
       val whites  = new Array[Int](board.connections - 1)
       val blacks  = new Array[Int](board.connections - 1)
-      groups.foreach { tgroup =>
+      board.groups.foreach { tgroup =>
         tgroup.player match {
           case White => whites(tgroup.count - 1) += 1
           case Black => blacks(tgroup.count - 1) += 1
--- cofoga-core/src/test/scala/cofoga/utility/NaiveUtilitySpec.scala
+++ cofoga-core/src/test/scala/cofoga/utility/NaiveUtilitySpec.scala
@@ -6,75 +6,22 @@ import Player._
 
 class NaiveUtilitySpecTest extends JUnit4(NaiveUtilitySpec)
 
-object NaiveUtilitySpec extends Specification //with NaiveUtility
+object NaiveUtilitySpec extends Specification with NaiveUtility
                                               with Logged {
-                                                /*
-  class TestBoard extends GameBoard { def m = matrix }
-  var board = new TestBoard()
-  var whites = new Array[Int](board.connections - 1)
-  var blacks = new Array[Int](board.connections - 1)
+  val board = new GameBoard() { def m = matrix }
 
-  def reset() = {
-    board = new TestBoard()
-    whites = new Array[Int](board.connections - 1)
-    blacks = new Array[Int](board.connections - 1)
-  }*/
-
-
-
-  /*
-
-  "horizontals" should { reset().before
-    "compute correct threats" in {
-      for (j <- 0 until board.cols if j % 2 == 0)
-        board.m(0)(j) = White
-      horizontalEvaluation(board, whites, blacks)
-      List(4, 0, 0) mustEqual whites.toList
-      List(0, 0, 0) mustEqual blacks.toList
-    }
-  }
-  "verticals calculation" should { reset().before
-    "compute correct threats" in {
-      for (i <- 0 until board.rows if i % 2 == 0)
-        board.m(i)(0) = White
-      verticalEvaluation(board, whites, blacks)
-      List(3, 0, 0) mustEqual whites.toList
-      List(0, 0, 0) mustEqual blacks.toList
-    }
-  }
-  "diagonally up calculation" should { reset().before
-    "compute correct threats" in {
-      board.m(0)(0) = White
-      board.m(2)(2) = White
-      board.m(4)(4) = White
-      diagupEvaluation(board, whites, blacks)
-      List(3, 0, 0) mustEqual whites.toList
-      List(0, 0, 0) mustEqual blacks.toList
-    }
-  }
-  "diagonally down calculation" should { reset().before
-    "compute correct threats" in {
-      board.m(5)(0) = White
-      board.m(3)(2) = White
-      board.m(1)(4) = White
-      diagdownEvaluation(board, whites, blacks)
-      List(3, 0, 0) mustEqual whites.toList
-      List(0, 0, 0) mustEqual blacks.toList
-    }
-  }
-  "utility function" should { reset().before
+  "utility function" should { board.reset().before
     "compute correct score" in {
       board.move(0, 0, 1, 1, 2, 2)
-      utility(board) mustEqual 0
+      utility mustEqual 0
     }    
     "compute correct score" in {
       board.move(0, 0, 1, 1, 2)
-      utility(board) mustEqual 9
+      utility mustEqual 9
     }
     "compute correct score" in {
       board.move(0, 0, 2, 0, 4, 0)
-      utility(board) mustEqual -6
+      utility mustEqual -6
     }
   }
-  */
 }
