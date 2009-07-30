b3761fc51dbf57da9b9d96db529c77210fbc47f8
Min Huang <min.huang@alumni.usc.edu>
2009-07-29 17:45:41 -0700

    Matrix will be part of IF

--- cofoga-core/src/main/scala/cofoga/ThreatGroups.scala
+++ cofoga-core/src/main/scala/cofoga/ThreatGroups.scala
@@ -5,10 +5,9 @@ import scala.collection.mutable.HashMap
 trait ThreatGroups {
   type Point = Pair[Int, Int]
   type ThreatGroup = Array[Point]
-  val board: GameBoard
   protected lazy val threats = {
     val map = new HashMap[Point, Threats]()
-    for (i <- 0 until board.rows; j <- 0 until board.cols) {
+    for (i <- 0 until rows; j <- 0 until cols) {
       val ts = directions.map(f => f(i)(j).toArray)
                          .filter(_.size == board.connections)
                          .toArray
@@ -20,22 +19,22 @@ trait ThreatGroups {
   def directions = List(lr _, du _, ur _, dr _)
 
   private def lr(row: Int)(col: Int) = {
-    for (j <- col until col + board.connections if board.containsCol(j))
+    for (j <- col until col + connections if containsCol(j))
       yield (row, j)
   }
 
   private def du(row: Int)(col: Int) = {
-    for (i <- row until row + board.connections if board.containsRow(i))
+    for (i <- row until row + connections if containsRow(i))
       yield (i, col)
   }
 
   private def ur(row: Int)(col: Int) = {
-    for (k <- 0 until board.connections if board.contains(row + k)(col + k))
+    for (k <- 0 until connections if contains(row + k)(col + k))
       yield (row + k, col + k)
   }
 
   private def dr(row: Int)(col: Int) = {
-    for (k <- 0 until board.connections if board.contains(row - k)(col + k))
+    for (k <- 0 until connections if contains(row - k)(col + k))
       yield (row - k, col + k)
   }
 }
