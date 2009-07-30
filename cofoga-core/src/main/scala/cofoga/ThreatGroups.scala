a236ffc7fbba5c22040bb1a82ec60bc8d0ef6fcf
Min Huang <min.huang@alumni.usc.edu>
2009-07-29 18:49:59 -0700

    Solved and unsolved group calculation

--- cofoga-core/src/main/scala/cofoga/ThreatGroups.scala
+++ cofoga-core/src/main/scala/cofoga/ThreatGroups.scala
@@ -1,40 +1,46 @@
 package cofoga
 
 import scala.collection.mutable.HashMap
+import scala.collection.mutable.HashSet
 
 trait ThreatGroups extends Matrix {
   type Point = Pair[Int, Int]
   type ThreatGroup = Array[Point]
 
   protected lazy val groups = {
-    val map = new HashMap[Point, Array[ThreatGroup]]()
+    val _groups = new HashMap[Point, Array[ThreatGroup]]()
+    val directions = List(lr _, du _, ur _, dr _)
     for (i <- 0 until rows; j <- 0 until cols) {
-      val ts = directions.map(f => f(i)(j).toArray)
-                         .filter(_.size == connections)
-                         .toArray
-      map += (i, j) -> ts
+      val tgroups = directions.map(d => d(i)(j).toArray).filter(_.size == connections).toArray
+      _groups += (i, j) -> tgroups
     }
-    map
+    Map() ++ _groups
   }
 
-  protected  def directions = List(lr _, du _, ur _, dr _)
+  protected lazy val unsolvedGroups = {
+    val _groups = new HashMap[Point, Array[ThreatGroup]]()
+    groups.foreach { t => _groups += t }
+    _groups
+  }
 
-  protected  def lr(row: Int)(col: Int) = {
+  protected lazy val solvedGroups = new HashMap[Point, Array[ThreatGroup]]()
+  
+  protected def lr(row: Int)(col: Int) = {
     for (j <- col until col + connections if containsCol(j))
       yield (row, j)
   }
 
-  protected  def du(row: Int)(col: Int) = {
+  protected def du(row: Int)(col: Int) = {
     for (i <- row until row + connections if containsRow(i))
       yield (i, col)
   }
 
-  protected  def ur(row: Int)(col: Int) = {
+  protected def ur(row: Int)(col: Int) = {
     for (k <- 0 until connections if contains(row + k)(col + k))
       yield (row + k, col + k)
   }
 
-  protected  def dr(row: Int)(col: Int) = {
+  protected def dr(row: Int)(col: Int) = {
     for (k <- 0 until connections if contains(row - k)(col + k))
       yield (row - k, col + k)
   }
